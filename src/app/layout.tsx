afe4518d718c699b49b374606a1429f07ea5536c
HaseeburRehma <jhaseeb718@gmail.com>
2025-08-08 13:37:52 -0400

    final fixes V1.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       className={inter.className}
       suppressHydrationWarning
     >
-      <head /> 
+      
       <body suppressHydrationWarning>
         <Providers>
           <SettingsProvider>
