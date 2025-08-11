69bb27e1f73a674c6b66cbcf1bb9e0e856adbb4f
HaseeburRehma <jhaseeb718@gmail.com>
2025-08-11 05:31:06 -0400

    page router  fix

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+// src/pages/404.tsx
+export default function NotFound() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <h1>404 – Page Not Found</h1>
+    </div>
+  )
+}
