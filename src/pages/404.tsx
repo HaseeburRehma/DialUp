a67a05f024327afe7330991acded2ed5100aad4d
HaseeburRehma <jhaseeb718@gmail.com>
2025-08-11 09:42:11 -0400

    Remove Pages Router; use App Router only

--- src/pages/404.tsx
+++
@@ -1,9 +0,0 @@
-// pages/404.tsx
-export default function NotFound() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>404 – Page not found</h1>
-      <p>Sorry, we couldn’t find that page.</p>
-    </div>
-  );
-}
