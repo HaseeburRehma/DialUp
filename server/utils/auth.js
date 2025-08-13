36caa95ecd0357f455e9a083ea13d8f6d22e82b8
HaseeburRehma <jhaseeb718@gmail.com>
2025-08-13 08:23:54 -0400

    build setup

---
+++ server/utils/auth.js
@@ -0,0 +1,14 @@
+const bcrypt = require("bcrypt");
+
+async function hashPassword(pw) {
+  return bcrypt.hash(pw, 10);
+}
+
+async function verifyPassword(plain, hash) {
+  return bcrypt.compare(plain, hash);
+}
+
+module.exports = {
+  hashPassword,
+  verifyPassword
+};
--- server/utils/auth.ts
+++
@@ -1,4 +0,0 @@
-// server/utils/auth.ts
-import bcrypt from "bcrypt"
-export async function hashPassword(pw: string) { return bcrypt.hash(pw, 10) }
-export async function verifyPassword(plain: string, hash: string) { return bcrypt.compare(plain, hash) }
