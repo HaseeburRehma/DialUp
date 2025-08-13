f1e8303646067ea4058859aa704574c05c57e9c4
HaseeburRehma <jhaseeb718@gmail.com>
2025-08-13 09:51:43 -0400

    build setup .

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,25 +1,23 @@
-// server/utils/db.js
-const mongoose = require('mongoose')
+import mongoose from 'mongoose'
 
 let cached = global._mongo
 if (!cached) {
   cached = global._mongo = { conn: null, promise: null }
 }
 
-async function connect() {
+export async function connect() {
   if (cached.conn) return cached.conn
   if (!cached.promise) {
     const uri = process.env.MONGODB_URI
     if (!uri) throw new Error('Missing MONGODB_URI environment variable')
     console.log('→ MONGODB_URI is', uri)
     cached.promise = mongoose.connect(uri, {
-      useNewUrlParser:    true,
+      useNewUrlParser: true,
       useUnifiedTopology: true,
     }).then(m => m)
   }
   cached.conn = await cached.promise
   return cached.conn
 }
-const db = mongoose.connection;
 
-module.exports = { connect, db };
+export const db = mongoose.connection
